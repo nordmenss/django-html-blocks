418042e0f0145a4629448854aa19c1d3192f7282
nordmenss <nordmenss@gmail.com>
2011-02-05 10:00:24 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,49 +1,24 @@
 #!/usr/bin/env python
 from distutils.core import setup
-import os
+from html_blocks import VERSION
 path='html_blocks'
-from path import VERSION
 
-long_description = open('README.txt').read()
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
+setup(name=path,
+      version=VERSION,
+      description='html blocks for django templates',
+      long_description = open('README.txt').read(),
+      author='NORD',
+      author_email='nordmenss@gmail.com',
+      url='https://github.com/nordmenss/django-html-blocks',
+      packages=[ path, ],
 
-for dirpath, dirnames, filenames in os.walk(path):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[(len(path)+1):] # Strip "admin_tools/" or "admin_tools\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
-setup(
-    name='django-html-blocks',
-    version='0.0',
-    description='template html blocks with modeltranslation support',
-    long_description=long_description,
-    author='NORD',
-    author_email='nordmenss@gmail.com',
-    url='https://github.com/nordmenss/django-html-blocks',
-    package_dir={path: path},
-    packages=packages,
-    package_data={path: data_files},
-    classifiers=[
-        'Development Status :: 0 - Alpha',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
-    ],
-    install_requires=['django-ckeditor','django-modeltranslation'],
-    zip_safe=False,
-)
+      classifiers=(
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+        ),
+      license="GPL"
+     )
